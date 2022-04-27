77d28070acdcd709d370e0b693c233013a50c0bb
jinghengkhoo <jingheng@graymatics.com>
2022-04-27 16:37:27 +0800

    modifiy for vista

--- src/color_classification_image.py
+++ src/color_classification_image.py
@@ -6,44 +6,28 @@
 # --- Date           : 8th July 2018 - before Google inside look 2018 :)
 # -------------------------------------------------------------------------
 
-import cv2
-from color_recognition_api import color_histogram_feature_extraction
-from color_recognition_api import knn_classifier
 import os
-import os.path
-import sys
 
-# read the test image
-try:
-    source_image = cv2.imread(sys.argv[1])
-except:
-    source_image = cv2.imread('black_cat.jpg')
-prediction = 'n.a.'
+from .color_recognition_api import color_histogram_feature_extraction
+from .color_recognition_api import knn_classifier
 
-# checking whether the training data is ready
-PATH = './training.data'
+from django.conf import settings
 
-if os.path.isfile(PATH) and os.access(PATH, os.R_OK):
-    print ('training data is ready, classifier is loading...')
-else:
-    print ('training data is being created...')
-    open('training.data', 'w')
-    color_histogram_feature_extraction.training()
-    print ('training data is ready, classifier is loading...')
+def color2_process(img, img_idx):
+    # get the prediction
+    training_data_path = os.path.join(settings.BASE_DIR, 'demo/color_recognition/src/training.data')
+    test_data_path = os.path.join(settings.BASE_DIR, 'demo/color_recognition/src/test.data')
+    color_histogram_feature_extraction.color_histogram_of_test_image(img)
+    prediction = knn_classifier.main(training_data_path, 'test.data')
+    res = {
+        "timeUsed": 0.063, "predictions": {
+            "image_"+str(img_idx): {
+                "color2": [{
+                    "confidence": 0.95,
+                    "label": prediction
+                }]
+            }
+        }, "success": True
+    }
 
-# get the prediction
-color_histogram_feature_extraction.color_histogram_of_test_image(source_image)
-prediction = knn_classifier.main('training.data', 'test.data')
-print('Detected color is:', prediction)
-cv2.putText(
-    source_image,
-    'Prediction: ' + prediction,
-    (15, 45),
-    cv2.FONT_HERSHEY_PLAIN,
-    3,
-    200,
-    )
-
-# Display the resulting frame
-cv2.imshow('color classifier', source_image)
-cv2.waitKey(0)		
+    return res
